9cea4c4e31c59e07c47ffcf44e426fc33e5fbca1
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-09-07 12:23:55 +0900

    Add files via upload

---
+++ fish.js
@@ -0,0 +1,11 @@
+var i = 0
+while ( i < 100 ) {
+document.write ('커뮤에서 놀지말고 작품이나 만드세여<br>')
+i = i + 1
+}
+
+function work() {
+location.replace ('https://playentry.org/ws')
+}
+
+setTimeout (work, 1000)
