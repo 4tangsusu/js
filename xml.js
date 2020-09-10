c4eae21aca3fa3f08c453b62b9dd39d3c204cb4a
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-09-10 15:55:02 +0900

    Update xml.js

--- xml.js
+++ xml.js
@@ -1,7 +1,7 @@
 function xmlParser(xml, tag) {
 let front = '<' + tag + '>';
 let start = xml.indexOf(front);
-let end = xml.indexOf('</' + tag + '>');
+let end = xml.indexOf('</' + tag + '>')
 
 if (start > -1, end > start) {
 return xml.substring(start + front.length, end) }
