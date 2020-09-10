99166ae6b75589e26298bb32b73b61b508db3c58
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-09-10 15:51:49 +0900

    Add files via upload

---
+++ xml.js
@@ -0,0 +1,8 @@
+function xmlParser(xml, tag) {
+let front = '<' + tag + '>';
+let start = xml.indexOf(front);
+let end = xml.indexOf('</' + tag + '>');
+
+if (start > -1, end > start) {
+return xml.substring(start + front.length, end) }
+}
