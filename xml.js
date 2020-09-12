1d164895055d3569db3a4b275fc0994ab4b157b0
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-09-12 20:30:50 +0900

    Update xml.js

--- xml.js
+++ xml.js
@@ -2,7 +2,7 @@ function xmlParser(xml, tag) {
 if (xml.includes('<' + tag + '>'),
 xml.indexOf('<' + tag + '>') < xml.indexOf('</' + tag + '>')
 ) {
-return(xml.substring(
+return(xml.slice(
 xml.indexOf('<' + tag + '>') + tag.length + 2,
 xml.indexOf('</' + tag + '>') - 1)
 ) }
