98137002496bdd33de811cd12a5ca31261eeb632
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-10-03 16:11:56 +0900

    Delete xml.js

--- xml.js
+++
@@ -1,9 +0,0 @@
-function xmlParser(xml, tag) {
-if (xml.includes('<' + tag + '>'),
-xml.indexOf('<' + tag + '>') < xml.indexOf('</' + tag + '>')
-) {
-return(xml.slice(
-xml.indexOf('<' + tag + '>') + tag.length + 2,
-xml.indexOf('</' + tag + '>') - 1)
-) }
-}
