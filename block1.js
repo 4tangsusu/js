4acec1900babdd30a44c76bf3268bf1604b314cb
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2020-10-03 16:54:13 +0900

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,12 +1,7 @@
 var username = prompt('정지되었는지 확인할 유저의 이름을 입력해주세요');
-if (await (
-  await fetch('https://playentry.org/api/getUserByUsername/'+username)
-).ok) {
-  if (await (
-    await (
-      await fetch('https://playentry.org/api/getUserByUsername/'+username)
-    ).json()
-  ).isBlocked) {
+var data = await fetch('https://playentry.org/api/getUserByUsername/'+username)
+if (await data.ok) {
+  if (await (await data.json()).isBlocked) {
       console.log('해당 유저는 정지되었습니다.');
   } else {
       console.log('해당 유저는 정지되지 않았습니다.');
